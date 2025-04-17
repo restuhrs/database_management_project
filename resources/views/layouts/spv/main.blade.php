0abf333d5aff60bf790e46e7606b866e3e13d8d5
Restu <restukharisma858@gmail.com>
2025-04-17 22:55:57 +0700

    add hover

--- resources/views/layouts/spv/main.blade.php
+++ resources/views/layouts/spv/main.blade.php
@@ -172,7 +172,7 @@
                     </li>
                     @else
                     <li class="text-sm leading-normal">
-                        <a class="font-bold text-blue-600" href="{{ route('spv.dashboard') }}">{{ $crumb }}</a>
+                        <a class="font-bold text-blue-600 transition-colors hover:text-blue-800" href="{{ route('spv.dashboard') }}">{{ $crumb }}</a>
                     </li>
                     @endif
                     @endforeach
